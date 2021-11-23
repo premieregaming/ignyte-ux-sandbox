fcf1842dc1fe3dd613d0aba32b3b86cf03ce4398
kenreilly <kenneth.reilly@hotmail.com>
2021-11-22 17:08:26 -0700

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
+    app.enableCors()
 
     await app.listen(process.env.DEPLOYMENT == 'STAGE' ? process.env.PORT : 5000);
     console.log(`Application is running on: ${await app.getUrl()}`);
